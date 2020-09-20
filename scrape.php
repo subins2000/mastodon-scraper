04866652a73579d50fa2c58f5f70a51d854b56b3
Subin Siby <mail@subinsb.com>
2020-09-21 03:42:01 +0530

    Add scrape script

---
+++ scrape.php
@@ -0,0 +1,36 @@
+<?php
+// Start config
+
+$instance = 'https://mastodon.social';
+$dump = 'dump.txt'; // Content will be appended
+$limit = 1000;
+$older_than_toot = ""; // Toot ID. Fetch toots after this. 
+
+// End config
+
+if ($older_than_toot !== "") {
+  $extra = "&max_id=$older_than_toot";
+} else {
+  $extra = "";
+}
+$fetched = [];
+
+while (count($fetched) < $limit ) {
+  $r = json_decode(file_get_contents("$instance/api/v1/timelines/public?local=true" . $extra), true);
+  foreach($r as $item) {
+    $id = $item['id'];
+
+    if (in_array($id, $fetched)) {
+      continue;
+    }
+
+    $data = "\n" . $item['id'] . "\n" . $item['content'] . "\n";
+    file_put_contents($dump, $data, FILE_APPEND);
+
+
+    $fetched[] = $id;
+  }
+  // older than
+  $extra = "&max_id=" . $id;
+}
+echo "Fetched " . count($fetched) . " toots.";
